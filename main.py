6f8d5977ad2ff521b67ca8109e32e51bcac9316e
Vitor Daniel <vitor036daniel@gmail.com>
2023-07-15 09:41:38 -0300

    adicionar visualizador de logs

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import aiodocker
+from collections import defaultdict
 from typing import Annotated, BinaryIO
-from fastapi import BackgroundTasks, FastAPI, Request, File, Form, UploadFile
+
+import aiodocker
+from fastapi import BackgroundTasks, FastAPI, File, Form, Request, UploadFile
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from prisma import Prisma
+from fastapi_socketio import SocketManager
 from slugify import slugify
+
+from prisma import Prisma
 
 docker = aiodocker.Docker()
 
@@ -14,7 +18,17 @@ prisma = Prisma()
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+socket = SocketManager(app=app)
+
 templates = Jinja2Templates(directory="templates")
+
+logs_map: dict[str, list[str]] = defaultdict(list)
+
+
+@socket.on("join")
+async def join(sid, data):
+    socket.enter_room(sid, data)
+    await socket.emit("build", logs_map[data], to=sid)
 
 
 @app.on_event("startup")
@@ -38,24 +52,28 @@ def deploy_page(request: Request):
     return templates.TemplateResponse("deploy.html.j2", {"request": request})
 
 
-async def deploy_service(name: str, file: BinaryIO, app_id: int):
+async def deploy_service(slug: str, file: BinaryIO, app_id: int):
     stream = docker.images.build(
         fileobj=file.file,
-        tag=name,
+        tag=slug,
         stream=True,
         encoding="utf-8"
     )
 
     async for line in stream:
-        print(line)
+        if line.get("stream"):
+            logs_map[slug].append(line["stream"])
+            await socket.emit("build", [line["stream"]], room=f"build_{slug}")
+        elif line.get("errorDetail"):
+            await socket.emit("error", line["errorDetail"]["message"], room=f"build_{slug}")
 
     service = await docker.services.create(
         task_template={
             "ContainerSpec": {
-                "Image": name
+                "Image": slug
             }
         },
-        name=name
+        name=slug
     )
 
     await prisma.service.create(
@@ -66,22 +84,34 @@ async def deploy_service(name: str, file: BinaryIO, app_id: int):
     )
 
 
-@app.post("/deploy", response_class=RedirectResponse)
+@app.post("/deploy", response_class=RedirectResponse, status_code=302)
 async def deploy(
     name: Annotated[str, Form()],
     file: Annotated[UploadFile, File()],
     background_tasks: BackgroundTasks
 ):
+    slug = slugify(name)
     app = await prisma.app.create(
         data={
             "name": name,
-            "slug": slugify(name)
+            "slug": slug
         }
     )
 
-    background_tasks.add_task(deploy_service, name, file, app.id)
+    background_tasks.add_task(deploy_service, slug, file, app.id)
 
-    return "/"
+    return f"/app/{app.slug}/build_logs"
+
+
+@app.get("/app/{slug}/build_logs", response_class=HTMLResponse)
+async def build_logs(request: Request, slug: str):
+    return templates.TemplateResponse(
+        "build_logs.html.j2",
+        {
+            "request": request,
+            "slug": slug
+        }
+    )
 
 
 if __name__ == "__main__":
